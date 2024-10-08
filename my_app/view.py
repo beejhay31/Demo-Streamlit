005429f72203e9fd47fc4ad8091473835bd68f02
beejhay31 <33355675+beejhay31@users.noreply.github.com>
2024-10-08 14:44:48 +0100

    Create view.py

---
+++ my_app/view.py
@@ -0,0 +1,7 @@
+class View:
+    def display_monitoring(self, reference_data, current_data):
+        st.write("Displaying monitoring results...")
+
+    def display_report(self, report, title):
+        st.subheader(title)
+        st.write(report)
