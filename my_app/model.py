db3f079dc0720c327f7c9d983a560b28d64df10b
beejhay31 <33355675+beejhay31@users.noreply.github.com>
2024-10-08 14:44:23 +0100

    Create model.py

---
+++ my_app/model.py
@@ -0,0 +1,15 @@
+class Model:
+    def train_model(self, reference_data, current_data):
+        st.write("Training model on selected data...")
+
+    def performance_report(self, reference_data, current_data):
+        return "Model Performance Report"
+
+    def target_report(self, reference_data, current_data):
+        return "Target Drift Report"
+
+    def data_drift_report(self, reference_data, current_data):
+        return "Data Drift Report"
+
+    def data_quality_report(self, reference_data, current_data):
+        return "Data Quality Report"
