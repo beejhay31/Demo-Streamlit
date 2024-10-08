716e84c2b48a3f1ec5a4fce3a27dfc68bf25fa52
beejhay31 <33355675+beejhay31@users.noreply.github.com>
2024-10-08 17:17:11 +0100

    Update view.py

--- my_app/view.py
+++ my_app/view.py
@@ -3,14 +3,17 @@ import streamlit as st
 class View:
     @staticmethod
     def display_report(report, report_name):
+        
         """Display a report in the Streamlit app."""
-        st.subheader(report_name)
+        """st.subheader(report_name)
         
         # Check if the report is an instance of Evidently's Report class
         if hasattr(report, 'show'):
             report.show()  # Use the show method to display the report
         else:
-            st.write(report) 
+            st.write(report)""" 
+        st.write(f"{report_name}")
+        st.components.v1.html(report.get_html(), height=1000, scrolling=True)
             
     @staticmethod
     def display_monitoring(reference_data, current_data):
